38888e00d923d1a46bad24b0303aeee4d6f298fc
ceciliassis <cy_assis@hotmail.com>
2016-11-16 23:36:55 -0200

    -amend

---
+++ MD/RedeNeural/RNA/Main.java
@@ -0,0 +1,19 @@
+package RNA;
+
+/**
+ *
+ * @author cecilia
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+//CRIANDO UM ARQUIVO NOVO COM DADOS RANDOMICOS
+//    final DataReader dr = new DataReader();
+//    dr.randomFile();
+    final ANN ann = new ANN();
+    ann.run();
+
+  }
+
+}
